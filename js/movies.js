f55717b7238fe27bd295e54e606358a302429b75
kleaho <kleaho@gmail.com>
2016-10-11 19:01:52 -0700

    changed to 2 functions vs. 6

--- js/movies.js
+++ js/movies.js
@@ -1,41 +1,33 @@
 var startrekImg = document.querySelector("#tt0796366");
 
-startrekImg.addEventListener("click", getstarTrek);
-
-function getstarTrek(event) {
-  $.get("https://omdbapi.com?i=tt0796366", displayMovieDetails);
-}
+startrekImg.addEventListener("click", getMovie);
 
 var vImg = document.querySelector("#tt0434409");
 
-vImg.addEventListener("click", getV);
-
-function getV(event) {
-  $.get("https://omdbapi.com?i=tt0434409", displayMovieDetails);
-}
+vImg.addEventListener("click", getMovie);
 
 var fantasiaImg = document.querySelector("#tt0032455");
 
-fantasiaImg.addEventListener("click", getFantasia);
-
-function getFantasia(event) {
-  $.get("https://omdbapi.com?i=tt0032455", displayMovieDetails);
-}
+fantasiaImg.addEventListener("click", getMovie);
 
 var spiritedImg = document.querySelector("#tt0245429");
 
-spiritedImg.addEventListener("click", getSpirited);
-
-function getSpirited(event) {
-  $.get("https://omdbapi.com?i=tt0245429", displayMovieDetails);
-}
+spiritedImg.addEventListener("click", getMovie);
 
 var totoroImg = document.querySelector("#tt0096283");
 
-totoroImg.addEventListener("click", getTotoro);
+totoroImg.addEventListener("click", getMovie);
 
-function getTotoro(event) {
+function getMovie(event) {
+  var clickedMovie = event.target;
+  var imbdID = clickedMovie.imdbID;
+  var url = "https://omdbapi.com?i=" + imbdID;
   $.get("https://omdbapi.com?i=tt0096283", displayMovieDetails);
+  $.get("https://omdbapi.com?i=tt0245429", displayMovieDetails);
+  $.get("https://omdbapi.com?i=tt0032455", displayMovieDetails);
+  $.get("https://omdbapi.com?i=tt0434409", displayMovieDetails);
+  $.get("https://omdbapi.com?i=tt0796366", displayMovieDetails);
+
 }
 
 
@@ -46,3 +38,54 @@ function displayMovieDetails(results) {
   document.querySelector("#movie-runtime").textContent=results.Runtime;
   document.querySelector("#movie-plot").textContent=results.Plot;
   }
+
+
+
+// var startrekImg = document.querySelector("#tt0796366");
+//
+// startrekImg.addEventListener("click", getstarTrek);
+//
+// function getstarTrek(event) {
+//   $.get("https://omdbapi.com?i=tt0796366", displayMovieDetails);
+// }
+//
+// var vImg = document.querySelector("#tt0434409");
+//
+// vImg.addEventListener("click", getV);
+//
+// function getV(event) {
+//   $.get("https://omdbapi.com?i=tt0434409", displayMovieDetails);
+// }
+//
+// var fantasiaImg = document.querySelector("#tt0032455");
+//
+// fantasiaImg.addEventListener("click", getFantasia);
+//
+// function getFantasia(event) {
+//   $.get("https://omdbapi.com?i=tt0032455", displayMovieDetails);
+// }
+//
+// var spiritedImg = document.querySelector("#tt0245429");
+//
+// spiritedImg.addEventListener("click", getSpirited);
+//
+// function getSpirited(event) {
+//   $.get("https://omdbapi.com?i=tt0245429", displayMovieDetails);
+// }
+//
+// var totoroImg = document.querySelector("#tt0096283");
+//
+// totoroImg.addEventListener("click", getTotoro);
+//
+// function getTotoro(event) {
+//   $.get("https://omdbapi.com?i=tt0096283", displayMovieDetails);
+// }
+//
+//
+// function displayMovieDetails(results) {
+//   document.querySelector("#movie-title").textContent=results.Title;
+//   document.querySelector("#movie-year").textContent=results.Year;
+//   document.querySelector("#movie-rating").textContent=results.imdbRating;
+//   document.querySelector("#movie-runtime").textContent=results.Runtime;
+//   document.querySelector("#movie-plot").textContent=results.Plot;
+//   }
